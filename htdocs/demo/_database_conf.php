983a602535afc77c6b3bcf1b75e550ae796298e5
shimoda-lab <shimoda4@users.noreply.github.com>
2021-07-09 19:50:44 +0900

    ddd

--- htdocs/demo/_database_conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbServer = '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
-$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
-# MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
-
