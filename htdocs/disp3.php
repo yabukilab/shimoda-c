e38d8781b0a59a6195268f4de77775903b4314a7
aaaooouuu <s2242069ps@s.chibakoudai.jp>
2024-06-20 12:02:08 +0900

    test

--- htdocs/disp3.php
+++ htdocs/disp3.php
@@ -15,7 +15,7 @@
             $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sql='SELECT * FROM list';
+            $sql='SELECT * FROM shimodac.list';
             $stmt=$db->prepare($sql);
             $stmt->execute();
 
