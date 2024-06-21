9927c0eb04699b30707f180bbc73a9dfcbfe0dd0
aaaooouuu <s2242069ps@s.chibakoudai.jp>
2024-06-21 15:17:10 +0900

    test

--- mydb.sql
+++ mydb.sql
@@ -10,14 +10,10 @@ CREATE TABLE `list` (
   `price` varchar(5) NOT NULL,
   `stock` int(11) NOT NULL,
   PRIMARY KEY (`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- `list`テーブルへのデータ挿入
-INSERT INTO `list` (`number`, `name1`, `name2`, `price`, `stock`) VALUES
-(1, '教科書1', 'A社', '500', 25);
+);
 
 -- `yoyaku`テーブルの作成
 CREATE TABLE `yoyaku` (
   `day` date NOT NULL,
   `code` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
