9f7c70b17a7edbf2881809d5ca28b93a58656d5f
qmalu <s2242111ay@s.chibakoudai.jp>
2024-06-24 16:02:29 +0900

    修正

--- htdocs/delete.php
+++ htdocs/delete.php
@@ -8,7 +8,7 @@
     <h2>予約キャンセル</h2>
     <form method="post" action="delete_done.php">
         学籍番号を入力してください。<br />
-        <input type="text" name="student_number" style="width:200px" required><br /><br />
+        <input type="text" name="student_number" style="width:200px" pattern="\d{7}" title="7桁の学籍番号を入力してください" required><br /><br />
         <input type="button" onclick="history.back()" value="戻る">
         <input type="submit" value="削除">
     </form>
