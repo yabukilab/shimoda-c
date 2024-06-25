d68650cd4d49d69f074c4f531b5dee9ef1764337
aaaooouuu <s2242069ps@s.chibakoudai.jp>
2024-06-25 16:41:37 +0900

    test

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,17 +11,23 @@
             justify-content: center;
             min-height: 100vh;
             margin: 0;
-            font-size: 15em; 
+            font-size: 1.5em; /* 文字サイズを1.5倍に設定 */
         }
         .form-container {
             display: flex;
-            justify-content: space-between;
-            width: 20%; 
-            margin-bottom: 3em; 
+            justify-content: space-around; /* Changed to space-around for equal spacing */
+            width: 80%; /* フォームの幅を80%に設定 */
+            margin-bottom: 1em; /* マージンを少し減らす */
         }
         form {
-            display: inline;
+            display: inline-flex; /* Changed to inline-flex for better layout control */
             margin: 0;
+            width: 100%; /* フォームの幅を100%に設定 */
+        }
+        input[type="submit"] {
+            width: 100%; /* ボタンの幅を100%に設定 */
+            padding: 10px; /* パディングを追加してボタンを大きくする */
+            font-size: 1em; /* ボタンの文字サイズをデフォルトに戻す */
         }
     </style>
 </head>
