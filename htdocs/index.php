aadc3910d8953ba5d04d6c9a92c0c28c8e315dbf
aaaooouuu <s2242069ps@s.chibakoudai.jp>
2024-06-25 16:32:57 +0900

    デザインの変更

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,35 +1,49 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>商品一覧</title>
-	</head>
-	<body>
-		<?php
-
-			require_once '_database_conf.php';
-
-			print '<br>';
-			print '<form method="get" action="add.php" style="display:inline;">';
-			print '<input type="submit" value="予約">';
-			print '</form>';
-
-			print '<form method="get" action="delete.php" style="display:inline;">';
-			print '<input type="submit" value="キャンセル">';
-			print '</form>';
-
-			print '<form method="get" action="disp.php" style="display:inline;">';
-			print '<input type="submit" value="予約確認">';
-			print '</form>';
-
-			print '<br><br><br>';
-			print '<form method="get" action="disp2.php" style="display:inline;">';
-			print '<input type="submit" value="受付確認">';
-			print '</form>';
-
-			print '<form method="get" action="disp3.php" style="display:inline;">';
-			print '<input type="submit" value="在庫確認">';
-			print '</form>';
-        ?>
-	</body>
+<head>
+    <meta charset="UTF-8">
+    <title>商品一覧</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            margin: 0;
+            font-size: 1.5em; 
+        }
+        .form-container {
+            display: flex;
+            justify-content: space-between;
+            width: 80%; 
+            margin-bottom: 3em; 
+        }
+        form {
+            display: inline;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="form-container">
+        <form method="get" action="add.php">
+            <input type="submit" value="予約">
+        </form>
+        <form method="get" action="delete.php">
+            <input type="submit" value="キャンセル">
+        </form>
+        <form method="get" action="disp.php">
+            <input type="submit" value="予約確認">
+        </form>
+    </div>
+    <div class="form-container">
+        <form method="get" action="disp2.php">
+            <input type="submit" value="受付確認">
+        </form>
+        <form method="get" action="disp3.php">
+            <input type="submit" value="在庫確認">
+        </form>
+    </div>
+</body>
 </html>
