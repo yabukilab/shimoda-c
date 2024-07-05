db0ec49ac157c1b7c4bfbd2dc63eba105862cab0
aaaooouuu <s2242069ps@s.chibakoudai.jp>
2024-07-05 13:16:02 +0900

    dbこれで動作するはず

--- mydb.sql
+++ mydb.sql
@@ -1,18 +1,35 @@
-CREATE DATABASE IF NOT EXISTS shimodac;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-USE shimodac;
+-- データベース: `shimodac`
+--
+CREATE DATABASE IF NOT EXISTS `shimodac` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `shimodac`;
 
--- `list`テーブルの作成
+-- テーブルの構造 `list`
+
+DROP TABLE IF EXISTS `list`;
 CREATE TABLE `list` (
-  `number` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) NOT NULL,
   `name1` varchar(50) NOT NULL,
   `name2` varchar(30) NOT NULL,
   `price` varchar(5) NOT NULL,
-  `stock` int(11) NOT NULL,
-  PRIMARY KEY (`number`)
-);
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- `yoyaku`テーブルの作成
+-- テーブルのデータのダンプ `list`
+
+INSERT INTO `list` (`number`, `name1`, `name2`, `price`, `stock`) VALUES
+(1, '教科書1', 'A社', '1000', 120),
+(2, '教科書2', 'B社', '1500', 75),
+(3, '教科書3', 'B社', '2400', 100),
+(4, '教科書4', 'C社', '600', 140),
+(5, '教科書5', 'C社', '10000', 160),
+(6, '教科書6', 'D社', '3000', 130);
+-- テーブルの構造 `yoyaku`
+
+DROP TABLE IF EXISTS `yoyaku`;
 CREATE TABLE `yoyaku` (
   `day` date NOT NULL,
   `code` int(7) NOT NULL,
@@ -21,6 +38,25 @@ CREATE TABLE `yoyaku` (
   `number3` int(7) NOT NULL,
   `number4` int(7) NOT NULL,
   `number5` int(7) NOT NULL,
-  `hidden` int(1) NOT NULL,
-  PRIMARY KEY (`code`)
-);
+  `hidden` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- テーブルのデータのダンプ `yoyaku`
+
+INSERT INTO `yoyaku` (`day`, `code`, `number1`, `number2`, `number3`, `number4`, `number5`, `hidden`) VALUES
+
+-- テーブルのインデックス `list`
+
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`number`);
+
+-- テーブルのインデックス `yoyaku`
+
+ALTER TABLE `yoyaku`
+  ADD PRIMARY KEY (`code`);
+
+-- テーブルの AUTO_INCREMENT `list`
+
+ALTER TABLE `list`
+  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
