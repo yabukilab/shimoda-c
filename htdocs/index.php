0a4ef2da58bf0f3c1f9bf5f6f487986760c24609
H2342073 <s2342073vl@s.chibakoudai.jp>
2025-06-15 22:28:20 +0900

    0615

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,32 +4,43 @@ session_start();
 if (empty($_SESSION['index_err_msg'])) {
     $_SESSION['index_err_msg'] = "";}
 
-
 ////// 以降、HTMLのコード //////
 ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>ログイン</title>
-    </head>
+<html lang="ja">
 
-    <body>
-        <h2>ログイン</h2>
-        <form method="POST">
-            ユーザID:<br>
-            <input type="text" name="user_id"><br><br>
-            パスワード:<br>
-            <input type="password" name="user_pass"><br><br>
-            <button type="submit" name="login">ログイン</button>
-            <p><font color="red"><?php echo $_SESSION['index_err_msg']; ?></font></p><br>
-            <button type="submit" name="register">ユーザ登録はこちら</button>
-        </form>
-    </body>
+  <head>
+    <meta charset='utf-8' />
+    <title>（実験・演習用）</title>
+  </head>
+
+ <body>
+      <h2>ログイン</h2>
+      <form method="POST">
+          ユーザID:<br>
+          <input type="text" name="user_id"><br><br>
+          パスワード:<br>
+          <input type="password" name="user_pass"><br><br>
+          <button type="submit" name="login">ログイン</button>
+          <p><font color="red"><?php echo $_SESSION['index_err_msg']; ?></font></p><br>
+          <button type="submit" name="register">ユーザ登録はこちら</button>
+      </form>
+ </body>
+
 </html>
 
 <?php
 ////// 以降、PHPのコード //////
+   
+
+// DB接続情報（ここを共通化）
+$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
+$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
+$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
+$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+
 
 // ログインボタンが押された時の処理
 if (isset($_POST['login'])) {
@@ -40,8 +51,11 @@ if (isset($_POST['login'])) {
     }else{
         try {
             // データベースへの接続
-            $dsn = 'mysql:dbname=music_archive;host=127.0.0.1';
-            $dbh = new PDO($dsn, 'db_admin', 'admin');
+           $db = new PDO($dsn, $dbUser, $dbPass);
+            # プリペアドステートメントのエミュレーションを無効にする．
+           $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            # エラー→例外
+           $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // 入力されたIDのパスワード取得
             $sql = 'SELECT user_pass FROM user WHERE user_id = :user_id'; // SQL文を構成
@@ -51,7 +65,7 @@ if (isset($_POST['login'])) {
             $user_pass = $sth->fetch(); // 処理結果の取得
             
             // ログイン認証処理
-            if($user_pass!=0 && $_POST['user_pass'] == $user_pass['user_pass']) {
+            if($user_pass!=0 && password_verify($_POST['user_pass'], $user_pass['user_pass'])){
                 // ログイン成功時の処理
                 $_SESSION['user_id'] = $_POST['user_id']; // ログインIDを格納したセッション変数を定義
                 $_SESSION['index_err_msg'] = ""; // エラーメッセージの削除
@@ -64,7 +78,7 @@ if (isset($_POST['login'])) {
 
         // データベースへの接続に失敗した場合
         } catch (PDOException $e) {
-            print('データベースへの接続　に失敗しました:' . $e->getMessage());
+            print('データベースへの接続に失敗しました:' . $e->getMessage());
         die();
         }
     }
