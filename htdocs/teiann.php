59265e700159b4367f4ddbc9e64fbea421fdf005
2342093 <s2342093la@s.chibakoudai.jp>
2025-06-20 16:43:29 +0900

    kousinn

--- htdocs/teiann.php
+++ htdocs/teiann.php
@@ -85,14 +85,16 @@ try {
             <?php foreach ($ingredients as $ingredient): ?>
                 <option value="<?= htmlspecialchars($ingredient['ingredient_id']) ?>"><?= htmlspecialchars($ingredient['ingredient_name']) ?></option>
             <?php endforeach; ?>
-        </select>       
+        </select>     
+        <br>  
         <label for="ingredient_id">食材を選択してください:</label>
         <select name="ingredient_id" id="ingredient_id">
             <option value="">選択してください</option>
             <?php foreach ($ingredients as $ingredient): ?>
                 <option value="<?= htmlspecialchars($ingredient['ingredient_id']) ?>"><?= htmlspecialchars($ingredient['ingredient_name']) ?></option>
             <?php endforeach; ?>
-        </select>       
+        </select>  
+        <br>     
         <label for="ingredient_id">食材を選択してください:</label>
         <select name="ingredient_id" id="ingredient_id">
             <option value="">選択してください</option>
