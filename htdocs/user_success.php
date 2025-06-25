dd6829043619fbaf69eab6d4b5a0f74f2417f3ec
Takumiito3 <s2342021cu@s.chibakoudai.jp>
2025-06-25 23:46:44 +0900

    ページ追加

---
+++ htdocs/user_success.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン完了ページ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>ログインに成功しました</h2>
+    <div class="button">
+      <a href="TOP.php">TOP画面</a>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
