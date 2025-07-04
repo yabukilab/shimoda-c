5cf06ccf91b5f1412482b577f868ac701fb146ff
Takumiito3 <s2342021cu@s.chibakoudai.jp>
2025-07-04 13:58:00 +0900

    修正

--- htdocs/admin_success.php
+++ htdocs/admin_success.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン完了ページ</title>
+    <title>管理者TOP画面</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
